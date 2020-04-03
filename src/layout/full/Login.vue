29e207d37598df700d7a93658f2268e90075c6c0
Tuyen Nguyen (MOR) <tuyen.nguyenvan@mor.com.vn>
2020-04-03 15:04:37 +0700

    create login page

---
+++ src/layout/full/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-wrapper">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "@/views/components/Login/login.vue";
+import messages from '@/views/components/chat/messages.js';
+
+export default {
+  name: "ChatScreen",
+  components: {
+    Login,
+  },
+  data:() => ({
+	messages: messages,
+}),
+};
+</script>
---
+++ src/views/components/Login/login.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    day la login page
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+  messages: {
+    type: Array,
+    required: true
+  }
+};
+</script>
